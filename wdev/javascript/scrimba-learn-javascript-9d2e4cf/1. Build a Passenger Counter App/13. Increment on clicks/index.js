2fe1684f10856668f53d6bfa87e429bd30024fb2
avneeshlingwal9 <avneeshlingwal@gmail.com>
2024-10-27 17:34:07 +0530

    Cloned javascript repo

--- wdev/javascript/scrimba-learn-javascript-9d2e4cf/1. Build a Passenger Counter App/13. Increment on clicks/index.js
+++
@@ -1,6 +0,0 @@
-let toChange = document.getElementById("count-el");
-let count = 0;
-function increment() {
-  count++;
-  toChange.innerText = count;
-}
--- wdev/javascript/scrimba-learn-javascript-9d2e4cf/1. Build a Passenger Counter App/16. Display the count with innerText/index.js
+++
@@ -1,9 +0,0 @@
-// grab the count-el element, store it in a countEl variable
-let count = 0;
-let change = document.getElementById("count-el");
-
-function increment() {
-  count = count + 1;
-  change.innerText = count;
-  // set countEl's innerText to the count
-}
